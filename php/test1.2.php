02fb13e590b36c968217466c69adeb7c1fc4cec8
samost <abusecheat@mail.ru>
2022-09-15 16:29:00 +0300

    fix

--- php/test1.2.php
+++ php/test1.2.php
@@ -1,1 +1,15 @@
 <?php
+$a = 1;
+$b = -2;
+$c = -24;
+
+function kr1($ds,$a,$b){
+    return ((-$b + sqrt($ds)) / 2 * $a);}
+function kr2($ds,$a,$b){
+    return ((-$b - sqrt($ds)) / 2 * $a);}
+function dis($a,$b,$c){
+    return ($b*$b - 4*$a*$c);}
+$ds = dis($a,$b,$c);
+echo "Дискриминант" ."&nbsp;" . $ds . "<br>";
+echo "Первый корень" . "&nbsp;" . (kr1($ds, $a,$b)) . "<br>";
+echo "Второй корень" . "&nbsp;" . (kr2($ds, $a,$b));
