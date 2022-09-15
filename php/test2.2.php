da0fb3279ec61eb0d32be2b7231656e401feb7ed
samost <abusecheat@mail.ru>
2022-09-15 18:35:28 +0300

    dx

---
+++ php/test2.2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>calc</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<form action = "/test2.1.php" method = "post">
+    <img src="/send.php?id=1">
+    <br>
+
+</form>
+</body>
+
+</html>
